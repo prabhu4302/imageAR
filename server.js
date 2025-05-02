883bf68f6f21088be83acc1391913be8372a1f11
prabhu4302 <prabhusubbiah@gmail.com>
2025-05-02 16:38:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const TEXTS_FILE = path.join(__dirname, 'data', 'texts.json');
 
 app.use(express.static('public'));
 app.use(express.json());
-app.listen(3000, '0.0.0.0');
 
 // Ensure data folder exists
 if (!fs.existsSync(TEXTS_FILE)) {
